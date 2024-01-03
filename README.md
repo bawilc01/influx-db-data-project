c5351cdd0cc966f58a3016827d13b2dd7fb376de
Brittney Coble <bwilcutt#gmail.com>
2024-01-03 00:48:03 -0500

    setting up live write

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Doing something with influxdb.
+
+reference material = https://dganais.medium.com/getting-started-with-python-and-influxdb-v2-0-f22e5175aba5
--- app/influx-write.py
+++ app/influx-write.py
@@ -1,24 +1,49 @@
-from dotenv import load_dotenv
-from pathlib import Path
-
-dotenv_path = Path('path/to/.env')
-load_dotenv(dotenv_path=dotenv_path)
-
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
-from influxdb_client import WritePrecision, InfluxDBClient, Point
+from influxdb_client import WritePrecision, InfluxDBClient, Point, WriteOptions
 from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.write_api import ASYNCHRONOUS
 
-url = 'https://us-west-2-1.aws.cloud2.influxdata.com'
-token = 'my-token'
-org = 'my-org'
-bucket = 'my-bucket'
+INFLUX_TOKEN = os.getenv('INFLUX_TOKEN')
+INFLUX_ORG = os.getenv('INFLUX_ORG')
+INFLUX_BUCKET = os.getenv('INFLUX_BUCKET')
+INFLUX_URL = os.getenv('INFLUX_URL')
+load_dotenv()
 
-with InfluxDBClient(url=url, token=token, org=org) as client:
-    p = Point("weatherstation") \
-        .tag("location", "San Francisco") \
-        .field("temperature", 25.9) \
-        .time(datetime.utcnow(), WritePrecision.MS)
+url = INFLUX_URL
+token = INFLUX_TOKEN
+org = INFLUX_ORG
+bucket = INFLUX_BUCKET
 
-    with client.write_api(write_options=SYNCHRONOUS) as write_api:
-        write_api.write(bucket=bucket, record=p)
+from influxdb_client import InfluxDBClient
+client = InfluxDBClient(url="http://localhost:9999", token=token, org=org)
+
+# instantiate the write api
+write_api = client.write_api()
+
+write_api.write(bucket, org, [{"measurement": "h2o_feet", "tags": {"location": "coyote_creek"}, "fields": {"water_level": 1}, "time": 1}])
+write_api_sync = client.write_api(write_options=SYNCHRONOUS)
+write_api_async = client.write_api(write_options=ASYNCHRONOUS)
+
+# batching write options
+"""
+The default instance of WriteApi uses batching. You can specify the following batch parameters:
+
+batch_size: the number of data points to collect in a batch
+flush_interval: the number of milliseconds before the batch is written
+jitter_interval: the number of milliseconds to increase the batch flush interval by a random amount
+retry_interval: the number of milliseconds to retry unsuccessful write. The retry interval is used when the InfluxDB server does not specify “Retry-After” header
+"""
+write_api_batch = client.write_api(write_options=WriteOptions(batch_size=500, flush_interval=10_000, jitter_interval=2_000, retry_interval=5_000))
+
+
+# with InfluxDBClient(url=url, token=token, org=org) as client:
+#     p = Point("weatherstation") \
+#         .tag("location", "San Francisco") \
+#         .field("temperature", 25.9) \
+#         .time(datetime.utcnow(), WritePrecision.MS)
+#
+#     with client.write_api(write_options=SYNCHRONOUS) as write_api:
+#         write_api.write(bucket=bucket, record=p)
